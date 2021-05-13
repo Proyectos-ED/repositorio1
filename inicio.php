7affc8a493d087c9c545422c4aa63a84edf73a9e
Adrian George Olteanu <agolteanu01@educarex.es>
2021-05-13 09:57:27 +0200

    Version 5. No funcionan las tildes del Mint

--- inicio.php
+++ inicio.php
@@ -4,4 +4,5 @@ echo "<h2>ENTORNOS DE DESARROLLO</h2>";
 echo "Esta es mi primera página de php!";
 echo "Hoy hemos aprendido GIT!";
 echo "A ver si funciona ahora...";
+echo "Funciona, pero no reconoce saltos de linea, y parece que tampoco las tildes...";
 ?>
