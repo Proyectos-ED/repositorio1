20743e097422d7127fcf1e1f1c1d399ec2dd9e12
Adrian George Olteanu <agolteanu01@educarex.es>
2021-05-13 10:10:01 +0200

    Version 6. Siguen sin funcionar las lineas de salto o las tildes

--- inicio.php
+++ inicio.php
@@ -1,8 +1,8 @@
 <h1>APLICACIÓN WEB CICLOS FP</h1>
 <?php
 echo "<h2>ENTORNOS DE DESARROLLO</h2>";
-echo "Esta es mi primera página de php!";
-echo "Hoy hemos aprendido GIT!";
-echo "A ver si funciona ahora...";
-echo "Funciona, pero no reconoce saltos de linea, y parece que tampoco las tildes...";
+echo "Esta es mi primera página de php!\n";
+echo "Hoy hemos aprendido GIT!\n";
+echo "A ver si funciona ahora...\n";
+echo "Funciona, pero no reconoce saltos de linea, y parece que tampoco las tildes...\n";
 ?>
