e9ef9e5c5bf772c91e99b3182ad4e1bfcade8567
Adrian George Olteanu <agolteanu01@educarex.es>
2021-05-13 10:17:45 +0200

    Version 8. Por fin funciona todo, la madre que me parió.

--- inicio.php
+++ inicio.php
@@ -1,8 +1,11 @@
 <h1>APLICACIÓN WEB CICLOS FP</h1>
 <?php
 echo "<h2>ENTORNOS DE DESARROLLO</h2>";
-echo "Esta es mi primera página de php!\n";
-echo "Hoy hemos aprendido GIT!\n";
-echo "A ver si funciona ahora...\n";
-echo "Funciona, pero no reconoce saltos de línea, y parece que tampoco las tildes...\n";
+echo "Esta es mi primera página de php!";
+echo "<br>";
+echo "Hoy hemos aprendido GIT!";
+echo "<br>";
+echo "A ver si funciona ahora...";
+echo "<br>";
+echo "Funciona, pero no reconoce saltos de línea, y parece que tampoco las tildes...";
 ?>
